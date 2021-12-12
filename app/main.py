8185c357862787cc6aff66972393453afa5ff0c9
Javier <javip261097@gmail.com>
2021-12-12 11:49:08 +0100

    feat: configure main window and url list widget

--- app/main.py
+++ app/main.py
@@ -1,8 +1,62 @@
 from configparser import ConfigParser
+import sys
+import os
+
+from PyQt5.Qt import QApplication, QClipboard
+from PyQt5 import QtCore, QtWidgets, QtGui
+from PyQt5.QtWidgets import QWidget, QMainWindow, QVBoxLayout
+from PyQt5.QtCore import QSize
+
+from widgets.urlListWidget import UrlListWidget
 
 cfg = ConfigParser()
 cfg.read('etc/config.ini')
 
 downloader = cfg.get('BASECONFIG','baseDownloader')
 
-print(downloader)
+
+
+iconSrc = (os.path.dirname(os.path.realpath(__file__))) + os.path.sep + 'src' + os.path.sep + 'img' + os.path.sep + 'logo.png'
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        self.setWindowIcon(QtGui.QIcon(iconSrc))
+        self.setMinimumSize(QSize(690, 355))
+        self.setFixedSize(690, 355) 
+        self.setWindowTitle("NSTK soundcloud downloader") 
+
+        layout = QVBoxLayout()
+
+        widgets = [
+            UrlListWidget
+        ]
+
+        for w in widgets:
+            layout.addWidget(w())
+        
+        widget = QWidget()
+        widget.setLayout(layout)
+
+        # Set the central widget of the Window. Widget will expand
+        # to take up all the space in the window by default.
+        self.setCentralWidget(widget)
+
+
+stylesheet = """
+    QMainWindow {
+        background-image: url(""" + iconSrc + """); 
+        background-repeat: no-repeat; 
+        background-position: right;
+        background-color: black;
+    }
+"""
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    app.setWindowIcon((QtGui.QIcon(iconSrc)))
+    app.setStyleSheet(stylesheet)
+    mainWin = MainWindow()
+    mainWin.show()
+    sys.exit( app.exec_() )
+
---
+++ app/widgets/urlListWidget.py
@@ -0,0 +1,51 @@
+
+from PyQt5 import QtCore, QtWidgets, QtGui
+from PyQt5.Qt import QApplication
+from PyQt5.QtWidgets import  QWidget, QLabel, QPushButton, QPlainTextEdit, QVBoxLayout, QCheckBox
+from PyQt5.QtCore import  Qt
+
+class UrlListWidget(QWidget):
+    def __init__(self, *args, **kwargs):
+        QWidget.__init__(self, *args, **kwargs)
+        
+        self.setFixedWidth(420)
+        
+        self.layout = QVBoxLayout(self)
+
+        # Add fields to paste urls
+        url_list_textbox_label = QLabel(self)
+        url_list_textbox_label.setText("Lista de temas:")
+        url_list_textbox_label.setStyleSheet('color: white; font-size: 20px;')
+        url_list_textbox_label.move(10, 35)
+
+        self.auto_paste_from_clipboard = QCheckBox("Obtener enlaces automáticamente del portapapeles", self)
+        self.auto_paste_from_clipboard.setStyleSheet("QCheckBox:unchecked{ color: white; }QCheckBox:checked{ background: black ; color: white; }")
+
+        self.url_list = QPlainTextEdit(self)
+        self.url_list.setPlaceholderText("Enlace 1\nEnlace 2\nEnlace 3\n...")
+        self.url_list.move(10,70)
+        self.url_list.setFixedSize(400,200)
+        QApplication.instance().clipboard().dataChanged.connect(self.clipboardChanged)
+
+
+        # Add download button
+        
+        download_button = QPushButton(self)
+        download_button.setText("Descargar")
+        download_button.setFixedSize(120,35)
+        download_button.setStyleSheet('')
+        self.download_button = download_button
+
+
+        self.layout.addWidget(url_list_textbox_label, alignment=Qt.AlignLeft)
+        self.layout.addWidget(self.auto_paste_from_clipboard, alignment=Qt.AlignRight)
+        self.layout.addWidget(self.url_list, alignment=Qt.AlignLeft, ) 
+        self.layout.addWidget(self.download_button, alignment=Qt.AlignCenter) 
+
+    def clipboardChanged(self):
+        text = QApplication.instance().clipboard().text()
+        if self.auto_paste_from_clipboard.isChecked():
+            if "soundcloud" in text and text not in self.url_list.toPlainText():
+                self.url_list.insertPlainText(text + "\n")
+
+    
